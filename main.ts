7ec0e6894870e49da8d508ac3690830eb363e7a0
pc20211166 <115064843+pc20211166@users.noreply.github.com>
2022-10-12 14:10:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let yes = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
     basic.showNumber(2)
@@ -5,6 +6,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(4)
     basic.showNumber(5)
     basic.showNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    yes = 9
+    basic.showNumber(yes)
+    for (let index = 0; index < 9; index++) {
+        yes += -1
+        basic.showNumber(yes)
+    }
 })
 basic.forever(function () {
 	
